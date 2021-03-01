96d218ad6cfb0ba496f10b01b514238013decd51
David Riegler <david.riegler@edu.fh-joanneum.at>
2021-03-01 17:10:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+
+// read JSON object from file
+fs.readFile('user.json', 'utf-8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+
+    // parse JSON object
+    const user = JSON.parse(data.toString());
+
+    // print JSON object
+    console.log(user);
+});
+
+
+
+
+// create a JSON object
+const user = {
+    "id": 1,
+    "name": "John Doe",
+    "age": 22
+};
+
+// convert JSON object to string
+const data = JSON.stringify(user);
+
+// write JSON string to a file
+fs.writeFile('user.json', data, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+});
+
+
+
